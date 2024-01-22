b29fc09cd6790cdd0c48f3df0bed5dbb54649f81
jigishasurani <139609284+jigishasurani@users.noreply.github.com>
2024-01-22 20:42:16 +0530

    first commit

---
+++ src/components/expenseitem.js
@@ -0,0 +1,18 @@
+import ExpenseDate from './expensedate';
+import Card from './Card';
+import './expensedate.css';
+import './expenseitem.css'
+
+function ExpenseItem(props) {
+  return (
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
+      </div>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
